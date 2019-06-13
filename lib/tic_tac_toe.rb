c8cb0284b38e84d0cfe326ee84200513995a5466
Juri <ojohnnyss@yahoo.com>
2019-06-13 13:49:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,28 +5,28 @@ WIN_COMBINATIONS = [
     [0, 4, 8], [2, 4, 6]
   ]
   
-def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(index, current_player = "X")
+def move(board, index, current_player)
   board[index] = current_player
 end
 
-def position_taken?(index)
+def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-def valid_move?(index)
-  index.between?(0,8) && !position_taken?(index)
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn_count(board)
@@ -63,47 +63,67 @@ def turn(board)
   end
 end
 
-def won?
-  WIN_COMBINATIONS.each {|combo|
-    index_0 = combo[0]
-    index_1 = combo[1]
-    index_2 = combo[2]
+def won?(board)
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
 
     position_1 = board[index_0]
     position_2 = board[index_1]
     position_3 = board[index_2]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return combo
+      return win_combo
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return combo
+      return win_combo
     end
   }
   return false
 end
 
-def full?
+def full?(board)
   board.all? {|index| index == "X" || index == "O"}
 end
 
-def draw?
-  if !won? && full?
+def draw?(board)
+  if !won?(board) && full?(board)
     return true
   else
     return false
   end
 end
 
-def over?
-  if won? || draw?
+def over?(board)
+  if won?(board) || draw?(board)
     return true
   else
     return false
+  end
+end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
   end
 end
 
 def play(board)
   until over?(board) == true
     turn(board)
+  end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
   end
 end
